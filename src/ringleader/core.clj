916b33212a82b9d3953c5bce62775dee43d35f82
Nathan Witmer <nwitmer@gmail.com>
2012-06-28 22:36:50 -0600

    Simplify proxy server with partial application

--- src/ringleader/core.clj
+++ src/ringleader/core.clj
@@ -16,15 +16,13 @@
                  (deliver finished true))]
     ;; wait for either the upstream *or* downstream to close:
     (deref finished)))
-(defn proxy-server [in out]
+(defn proxy-server [downstream in out]
+  (println "connecting to proxy")
+  (create-client (partial proxy-streams in out) downstream))
 
-  (letfn [(proxy-to-client [client c-in c-out]
-            (proxy-streams in out c-in c-out))]
-    (println "connecting to proxy")
-    (create-client proxy-to-client 10001)))
 
 (defn -main [& args]
   ; (create-server echo-server 1234)
-  (create-server proxy-server 10000)
+  (create-server (partial proxy-server 10001) 10000)
   (println "server started."))
 
