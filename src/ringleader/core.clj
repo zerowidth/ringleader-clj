14cecc9250b9225c26225ae97b2b0f57f3cb7f0d
Nathan Witmer <nwitmer@gmail.com>
2012-06-28 22:39:38 -0600

    Fake app server with slow app "start"

--- src/ringleader/core.clj
+++ src/ringleader/core.clj
@@ -10,9 +10,39 @@
   (println "connecting to proxy")
   (create-client (partial proxy-streams in out) downstream))
 
+(defn start-app [{:keys [starter started]}]
+  (let [me (.getId (Thread/currentThread))]
+    (println me "getting app")
+    (if (= me (swap! starter (fn [id] (or id me))))
+      (do
+        (println me "starting app...")
+        (Thread/sleep 10000)
+        (println me "app started!")
+        (deliver started true))
+      (do
+        (println me "lost the race, waiting for start...")
+        (deref started)))))
+
+(defn app-connection [app in out]
+  (let [me (.getId (Thread/currentThread))]
+    (println me "app connection")
+    (when (nil? (deref (:starter app)))
+      (println me "app not running")
+      (start-app app))
+    (println me "app started?")
+    (when (deref (:started app))
+      (println me "proxying!")
+      (create-client (partial proxy-streams in out) (:downstream app)))))
+
+(defn create-app-server [port downstream]
+  (let [app {:downstream downstream
+             :starter (atom nil)
+             :started (promise)}]
+    (create-server (partial app-connection app) port)))
 
 (defn -main [& args]
-  ; (create-server echo-server 1234)
-  (create-server (partial proxy-server 10001) 10000)
+  ; (create-server echo-server 10000)
+  ; (create-server (partial proxy-server 10001) 10000)
+  (create-app-server 10000 10001)
   (println "server started."))
 
