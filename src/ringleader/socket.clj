a2b85bb247045bab3cd6c283fcc9146329441c83
Nathan Witmer <nwitmer@gmail.com>
2012-07-02 19:57:32 -0600

    Rejigger socket docstrings and arguments

--- src/ringleader/socket.clj
+++ src/ringleader/socket.clj
@@ -10,7 +10,9 @@
   (doto (Thread. ^Runnable f)
     (.start)))
 
-(defn proxy-streams [upstream-in upstream-out downstream-in downstream-out]
+(defn proxy-streams
+  [upstream-in upstream-out downstream-in downstream-out]
+  "Proxy a pair of in/out streams to another pair of in/out streams."
   (let [finished (promise)]
     (on-thread #(do
                   (try
@@ -25,60 +27,79 @@
     ; wait for either the upstream *or* downstream to close
     (deref finished)))
 
-(defn- close-socket [^Socket s]
+(defn close-socket [^Socket s]
   (when-not (.isClosed s)
     (doto s
       (.shutdownInput)
       (.shutdownOutput)
       (.close))))
 
-(defn- accept-fn [^Socket s connections fun]
+(defn- accept-fn [^Socket s connections callback]
   (println "connection accepted")
   (let [in (.getInputStream s)
         out (.getOutputStream s)]
     (on-thread #(do
                   (dosync (commute connections conj s))
                   (try
-                    (fun in out)
+                    (callback in out)
                     (catch SocketException e))
                   (println "closing connection")
                   (close-socket s)
                   (dosync (commute connections disj s))))))
 
-(defn create-server [fun port]
+(defn create-server
+  [callback port]
+  "Create a socket server.
+
+   callback  - callback function for each new connection. Called with the input
+               and output streams for the connected socket. Runs in its own
+               thread, and the socket is closed when this function returns.
+   port      - what port the socket listens on"
+
   (let [server (ServerSocket. port)
         connections (ref #{})]
     (on-thread #(when-not (.isClosed server)
                   (try
-                    (accept-fn (.accept server) connections fun)
+                    (accept-fn (.accept server) connections callback)
                     (catch SocketException e))
                   (recur)))
     {:socket server :connections connections}))
 
 (defn close-server [server]
+  "Close a server and its connected clients
+
+   server - a server map with the :server-socket and :connections to close out."
   (doseq [s @(:connections server)]
     (close-socket s))
   (dosync (ref-set (:connections server) #{}))
   (.close ^ServerSocket (:server-socket server)))
 
 (defn connection-count [server]
+  "Returns how many clients are connected to a server"
   (count @(:connections server)))
 
-(defn- open-connection [host port]
+(defn open-connection [host port]
+  "Open a connection to host/port and return it, or return nil if unsuccessful"
   (try
     (Socket. host port)
     (catch SocketException e
       (println "could not connect to" host "on port" port))))
 
-(defn create-client [fun port]
+(defn create-client [callback port]
+  "Create a socket client.
+
+  callback - connection callback, called with the in/out streams for a connected
+             socket if the connection is successful. The socket connection is
+             closed when this function returns.
+  port     - what port to connect to"
+
   (if-let [s (open-connection "localhost" port)]
     (let [in (.getInputStream s)
           out (.getOutputStream s)]
       (try
-        (fun in out)
+        (callback in out)
         (catch SocketException e))
       (println "closing client connection")
       (close-socket s))
     (println "couldn't connect?")))
-
 
